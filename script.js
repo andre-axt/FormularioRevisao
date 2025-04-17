ceac5fa915b1d8822c67cdc8f770863e691dd2a6
andre-axt <andreaxtbachtold@gmail.com>
2025-04-17 12:46:47 -0300

    edit - validações no javascript

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function validarForm(event){
-    event.preventDefault();
+function validarForm(){
 
     const nome_validar = document.forms['formcadastro'].nome.value.length;
-    const email_validar = document.forms['formcadastro'].nome.value.length;
+    const sobrenome_validar = document.forms['formcadastro'].sobrenome.value.length;
+
 
     if(nome_validar < 5 || nome_validar > 50){
         alert('O nome precisa ter entre 6-49 caracteres e não pode ter números ou caracteres especiais.');
         return false;
     }  
-    if(!email_validar.includes('@') || email_validar < 20){
-        alert('Insira um email válido')
+    if(sobrenome_validar < 3 || sobrenome_validar > 50){
+        alert('O sobrenome precisa ter entre 4-49 caracteres e não pode ter números ou caracteres especiais.');
         return false;
-    } 
-     
+    }
+    
+    alert('Formulário enviado com sucesso')
 };
 
 document.getElementById('formcadastro').addEventListener('submit', validarForm );
